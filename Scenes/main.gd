091ca86e19a8884d62ae63e6d9a889217a438773
M0G1WARA <121844050+M0G1WARA@users.noreply.github.com>
2025-02-12 19:09:54 -0600

    Movimiento horizontal con temporizador

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var dragging:bool = false
 var chat_window:bool = false
+var moving:bool = false
+var direction:String = "left"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,8 +11,9 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	if moving:
+		move_window()
 
 
 func _input(event):
@@ -20,8 +23,11 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			dragging = true
+			moving = false
 		else:
 			dragging = false
+			if not chat_window:
+				$Timer.start()
 	
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 			chat()
@@ -30,13 +36,15 @@ func chat():
 	
 	if not chat_window:
 		chat_window = true
+		moving = false
+		$Timer.stop()
 	
 		var monitor_id = DisplayServer.window_get_current_screen()
 		var monitor_resolution = DisplayServer.screen_get_size(monitor_id)
 		var window_position = DisplayServer.window_get_position()
 		
-		var chat = load("res://Scenes/chat.tscn")
-		var instance = chat.instantiate()
+		var chat_scene = load("res://Scenes/chat.tscn")
+		var instance = chat_scene.instantiate()
 		var new_window = Window.new()
 		new_window.borderless = true
 		new_window.transparent = true
@@ -57,3 +65,34 @@ func chat():
 				child.queue_free()
 				break
 		chat_window = false
+		$Timer.start()
+
+
+func move_window():
+	var monitor_id = DisplayServer.window_get_current_screen()
+	var monitor_resolution = DisplayServer.screen_get_size(monitor_id)
+	var window_position = DisplayServer.window_get_position()
+
+	match direction:
+
+		"left":
+			if dragging == false and window_position.x < monitor_resolution.x-DisplayServer.window_get_size().x:
+				get_tree().root.position += Vector2i(1,0)
+
+			if window_position.x+DisplayServer.window_get_size().x >= monitor_resolution.x:
+				direction = "right"
+				moving = false
+				$Timer.start()
+
+		"right":
+			if dragging == false and window_position.x > 0:
+				get_tree().root.position -= Vector2i(1,0)
+
+			if window_position.x == 0:
+				direction = "left"
+				moving = false
+				$Timer.start()
+
+
+func _on_timer_timeout():
+	moving = true
